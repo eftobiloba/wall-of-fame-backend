e23b19ff5c7c618419690acebd1c2716c32d5c7b
Tobiloba Faseyitan <115575233+eftobiloba@users.noreply.github.com>
2024-07-15 02:43:13 -0700

    Update main.py to fix cors issue

--- app/main.py
+++ app/main.py
@@ -4,9 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "https://wall-of-fame-pi.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
